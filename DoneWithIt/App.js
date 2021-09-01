974044b43648ef5e75c0caf25de4a95d7372b84d
clu-codes <retsulcconnor@gmail.com>
2021-08-31 22:49:43 -0400

    create welcome screen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,42 +1,14 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ImageBackground, StyleSheet } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", // sets the main axis to be horizontal
-        justifyContent: "center", // centers the content based on the main axis - horizontal
-        alignItems: "center", // sets content based on the secondary axis
-        flexWrap: "wrap",
-        alignContent: "center", // determines the alignment of entire content - only works if you have wrapping
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          flexBasis: 100, // maps to width or height
-          flexGrow: 1, // same as setting flex property
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
+
+  // const styles = StyleSheet.create({
+  //   image: {
+  //     flex: 1,
+  //     justifyContent: "center",
+  //   },
+  // });
 }
